63b0aa37887edec80474dc9a44164116af41b1e0
saway11 <rgarcia20.m@gmail.com>
2023-05-25 14:41:55 -0700

    added questions, and function to gather user input

--- server.js
+++ server.js
@@ -18,5 +18,44 @@ const db = mysql.createConnection(
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
     },
-)
+    console.log('Connected to the employee tracker db.')
+);
+
+// Questions
+function askQuestion(isStartUp) {
+    const questions = [
+        {
+            type: 'input',
+            name: 'startUp',
+            message: 'Press any key to continue',
+            when: !isStartUp,
+        },
+        {
+            type: 'list',
+            name: 'likeToDo',
+            message: `What would you like to do`,
+            choices: [
+                'view all departments',
+                'view all roles',
+                'view all employees',
+                'add a department',
+                'add a role',
+                'add an employee',
+                'update and employee role',
+                'quit',
+            ],
+        },
+    ];
+
+    // function to gather user input and perfomr actions based on the selected choice.
+    inquirer
+        .createPromptModule(questions)
+        .then((data) => {
+            switch (data.likeToDo) {
+                case 'view all departments':
+                    viewAll(`SELECT department.id AS id, name AS department
+                    FROM department;`);
+            }
+        })
+}
 
