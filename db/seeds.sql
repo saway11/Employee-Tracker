678d411889de0dc9cff76793e5faddca637ef04c
saway11 <rgarcia20.m@gmail.com>
2023-05-26 10:51:30 -0700

    added roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,4 +5,14 @@ VALLUES ("Development"),
         ("Advertising");
 
 INSERT INTO role ( title,, salary, department_id )
-VALUES ("Tech Lead", 900000.00, 1),
+VALUES  ("Tech Lead", 900000.00, 1),
+        ("Front End Developers" ,310000.00, 1),
+        ("Software Developer" ,510000.00, 1),
+        ("Backend Developer" ,310000.00, 1),
+        ("Marketing Manager", 100000.00, 2),
+        ("Marketing Specialist" , 60000.00, 2),
+        ("Sales Manager" , 129000.00, 3),
+        ("Sales Rep" ,70000.00, 3),
+        ("Advertising Manager", 200000.00, 4),
+        ("Art Director" ,134000.00, 4),
+        ("Copy Writer" ,156000.00, 4);
