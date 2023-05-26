f8dcda2dcd43d6612d62cde885a999fe35f8a370
saway11 <rgarcia20.m@gmail.com>
2023-05-26 10:59:01 -0700

    added table

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,12 @@ CREATE TABLE role (
     FOREIGN KEY (deparment_id)
     REFERENCES department(id)
 );
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFER;ENCES role(id)
+)
