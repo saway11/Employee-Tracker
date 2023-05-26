e87eda52578ed15972739a0b3ac268a8ca288230
saway11 <rgarcia20.m@gmail.com>
2023-05-26 10:50:42 -0700

    added deparment names

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSET INTO department (name)
+VALLUES ("Development"),
+        ("Marketing"),
+        ("Sales"),
+        ("Advertising");
+
+INSERT INTO role ( title,, salary, department_id )
+VALUES ("Tech Lead", 900000.00, 1),
